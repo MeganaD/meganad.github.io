6bb57a5357f644641efc29f688b1492e92976989
meganad <meganad@outlook.kr>
2020-07-14 00:40:29 +0900

    mod cb

--- js/cb.js
+++ js/cb.js
@@ -2,21 +2,27 @@ let codes = document.querySelectorAll('code');
 let countID = 0;
 codes.forEach((code) => {
 
-  code.setAttribute("id", "code" + countID);
-  
-  let btn = document.createElement('button');
-  btn.innerHTML = "📋";
-  btn.className = "btn-cb";
-  btn.setAttribute("data-clipboard-action", "copy");
-  btn.setAttribute("data-clipboard-target", "#code" + countID);
-  
-  let div = document.createElement('div');
-  div.classNam = "cb-div";
-  div.appendChild(btn);
-  
-  code.before(div);
+    code.setAttribute("id", "code" + countID);
 
-  countID++;
-}); 
+    let btn = document.createElement('button');
+    btn.innerHTML = "Copy";
+    btn.className = "btn-cb";
+    btn.setAttribute("data-clipboard-action", "copy");
+    btn.setAttribute("data-clipboard-target", "#code" + countID);
+
+    let div = document.createElement('div');
+    div.className = "cb-div";
+    div.appendChild(btn);
+
+    if(code.parentElement.className == "highlight")
+        code.after(div);
+    else
+        code.before(div);
+
+    countID++;
+});
 
 let clipboard = new ClipboardJS('.btn-cb');
+// clipboard.on('success', function(e) {
+//     e.clearSelection();
+// });
