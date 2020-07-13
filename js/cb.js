86ecfa32ebf771f05a5915af262d57932585e4cc
meganad <meganad@outlook.kr>
2020-07-13 14:48:46 +0900

    cb

--- js/cb.js
+++ js/cb.js
@@ -2,10 +2,10 @@ let codes = document.querySelectorAll('.highlight > pre > code');
 let countID = 0;
 codes.forEach((code) => {
 	code.setAttribute("id", "code" + countID);
-	let clipButton = '<button class="btn" data-clipboard-target="#code' + countID 
+	let clipButton = '<button class="btn btn-sm cb_b" data-clipboard-target="#code' + countID 
 		+ '"><img src="/img/clippy.svg" width="13" alt="Copy to clipboard"></button>';
 	$(code).after(clipButton);
 	countID++; 
 }); 
 
-let clipboard = new ClipboardJS('.btn-copy');
+let clipboard = new ClipboardJS('.btn');
