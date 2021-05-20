9f2e2cea067fbc3f907566b656f245b69d1f86e2
meganad <meganad@outlook.kr>
2021-05-20 17:27:01 +0900

    .

--- js/swipe.js
+++ js/swipe.js
@@ -1,11 +1,12 @@
 $(function(){
     $("body").swipe( {
         swipeLeft:function(event, direction, distance, duration, fingerCount) {
-            console.log("You swiped " + direction );
+            var nextPage = document.querySelectorAll('li.next>a')[0];
+            if(nextPage) window.location.href = nextPage.href;
         },
         swipeRight:function(event, direction, distance, duration, fingerCount) {
-            console.log("You swiped " + direction );
+            var prevPage =  document.querySelectorAll('li.previous>a')[0];
+            if(prevPage) window.location.href = prevPage.href;
         },
       });
 });
-
