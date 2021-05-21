2177f1c6c04f4bde57e8c7283707e643cb9f64c9
meganad <meganad@outlook.kr>
2021-05-21 11:08:20 +0900

    .

--- js/swipe.js
+++ js/swipe.js
@@ -1,12 +1,18 @@
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
+
 $(function(){
-    $("body").swipe( {
-        swipeLeft:function(event, direction, distance, duration, fingerCount) {
-            var nextPage = document.querySelectorAll('li.next>a')[0];
-            if(nextPage) window.location.href = nextPage.href;
-        },
-        swipeRight:function(event, direction, distance, duration, fingerCount) {
-            var prevPage =  document.querySelectorAll('li.previous>a')[0];
-            if(prevPage) window.location.href = prevPage.href;
-        },
-      });
+    if(isMobile()){
+        $("body").swipe( {
+            swipeLeft:function(event, direction, distance, duration, fingerCount) {
+                var nextPage = document.querySelectorAll('li.next>a')[0];
+                if(nextPage) window.location.href = nextPage.href;
+            },
+            swipeRight:function(event, direction, distance, duration, fingerCount) {
+                var prevPage =  document.querySelectorAll('li.previous>a')[0];
+                if(prevPage) window.location.href = prevPage.href;
+            },
+        });
+    }
 });
